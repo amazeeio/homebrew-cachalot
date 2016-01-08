7129eae2d1df7cb491f46cffe0dc34c7ce1039ba
Brian Palmer <brian@codekitchen.net>
2016-01-08 09:18:28 -0700

    Create dinghy.rb

---
+++ Formula/dinghy.rb
@@ -0,0 +1,27 @@
+DINGHY_VERSION='4.1.0'
+
+class Dinghy < Formula
+  desc "faster, friendlier Docker on OS X"
+  homepage "https://github.com/codekitchen/dinghy"
+  url "https://github.com/codekitchen/dinghy.git", :tag => "v#{DINGHY_VERSION}"
+  version DINGHY_VERSION
+  head "https://github.com/codekitchen/dinghy.git", :branch => :master
+
+  depends_on "unfs3"
+  depends_on "dnsmasq"
+
+  def install
+    bin.install "bin/dinghy"
+    bin.install "bin/_dinghy_command"
+    prefix.install "cli"
+  end
+
+  def caveats; <<-EOS.undent
+    Run `dinghy create` to create the VM, then `dinghy up` to bring up the VM and services.
+    EOS
+  end
+
+  test do
+    system "dinghy", "version"
+  end
+end
