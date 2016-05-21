00672eeba9b065f94eb081c0b885c55865e918d0
Michael Schmid <michael@amazeelabs.com>
2016-05-21 10:24:42 +0200

    release v0.11.0

--- Formula/cachalot.rb
+++ Formula/cachalot.rb
@@ -1,7 +1,7 @@
 class Cachalot < Formula
   desc "Local OS X Drupal Hosting based on Docker"
   homepage "https://github.com/AmazeeIO/cachalot"
-  url "https://github.com/AmazeeIO/cachalot.git", :tag => "v0.10.2", :revision => "c76d93c6e5d32289a905494ccd31d9f34194d8b5"
+  url "https://github.com/AmazeeIO/cachalot.git", :tag => "v0.11.0", :revision => "87c59c7710b2d97ac92f18c2642dbf8efb5750e7"
   head "https://github.com/AmazeeIO/cachalot.git", :branch => :master
 
   depends_on "unfs3"
